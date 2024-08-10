726eb43449c05da310be43b31c6a26aeeb9b235e
tiago002pro <tiagobarbosa02@outlook.com>
2024-08-10 12:50:06 -0300

    Criação da navbar

---
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent implements OnInit {
+  tabsNavigate:any[] = []
+
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+    this.__createNavigateTabs()
+  }
+
+  private __createNavigateTabs():void {
+    this.tabsNavigate = [
+      { label: "Usuários", router: "user" },
+      { label: "Livros", router: "book" },
+      { label: "Empréstimos", router: "loan" },
+      { label: "Google Books", router: "" },
+    ]
+  }
+}
