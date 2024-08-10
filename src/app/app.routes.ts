4673a17f1f053047593f51ea98de96d4bd1c9bfa
tiago002pro <tiagobarbosa02@outlook.com>
2024-08-10 12:52:04 -0300

    Configurando a routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './screens/user/user.component';
+import { BookComponent } from './screens/book/book.component';
+import { LoanComponent } from './screens/loan/loan.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/user', pathMatch: 'full' },
+  { path: 'user', component: UserComponent },
+  { path: 'book', component: BookComponent },
+  { path: 'loan', component: LoanComponent },
+];
